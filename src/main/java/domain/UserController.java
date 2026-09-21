66eb7ea3fefd39312ae4426548b1cfcd117f4c4d
Gabby AK <gkadiga1@gmail.com>
2026-09-21 10:25:10 +0200

    updates i userservice og usercontroller

--- src/main/java/domain/UserController.java
+++ src/main/java/domain/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         if (user != null) {
             ctx.redirect("/map.html");
         } else {
-            ctx.status(401);
+            ctx.status(400);
             ctx.result("Forkert email eller adgangskode");
         }
     }
@@ -34,28 +34,37 @@ public class UserController {
         String phonenumber = ctx.formParam("phonenumber");
         String password = ctx.formParam("password");
 
-        if (email == null || !email.contains("@")) {
-            ctx.status(400);
-            ctx.result("Indtast en gyldig email.");
+        if (!validateUser(ctx, email, phonenumber, password)){
             return;
         }
-        if (password == null || !userService.validatePassword(password)) {
-            ctx.status(400);
-            ctx.result("Adgangskoden skal have mellem 8 og 15 tegn.");
-            return;
-        }
-        if (phonenumber == null || phonenumber.length() != 8) {
-            ctx.status(400);
-            ctx.result("Telefonnummer skal indeholde 8 cifre.");
-            return;
-        }
+
 
         User user = userService.createUser(email, phonenumber, password);
         if (user != null) {
             ctx.redirect("/map.html");
         } else {
-            ctx.status(409);
+            ctx.status(400);
             ctx.result("Der findes allerede en konto med denne email eller dette telefonnummer.");
         }
+    }
+
+    private static boolean validateUser(Context ctx, String email, String phonenumber, String password) {
+        if (email == null || !email.contains("@")) {
+            ctx.status(400);
+            ctx.result("Indtast en gyldig email.");
+            return false;
+        }
+        if (password != null && password.length() >= 8 && password.length() <= 15) {
+            ctx.status(400);
+            ctx.result("Adgangskoden skal have mellem 8 og 15 tegn.");
+            return false;
+        }
+        if (phonenumber == null || phonenumber.length() != 8) {
+            ctx.status(400);
+            ctx.result("Telefonnummer skal indeholde 8 cifre.");
+            return false;
+        }
+        return true;
+
     }
 }
--- src/main/java/domain/UserService.java
+++ src/main/java/domain/UserService.java
@@ -35,15 +35,9 @@ public class UserService {
     }
 
     public User login(String mail, String password) {
-        if (mail == null || password == null) {
-            System.out.println("no login!");
-            return null;
-        }
-
         for (User login : users) {
             if (login.getMail().equals(mail) &&
                     login.getPassword().equals(password)) {
-                System.out.println("Login succesful!");
                 return login;
             }
         }
@@ -52,23 +46,9 @@ public class UserService {
     }
 
     public User createUser(String mail, String telefon, String password) {
-        if (mail == null || telefon == null || password == null) return null;
-        if (!validatePassword(password)) return null;
-
-        for (User us : users) {
-            if (us.getMail().equals(mail) && us.getPhoneNumber().equals(telefon) && us.getPassword().equals(password)) {
-                return null;
-            }
-        }
-
         User user = new User(mail, telefon, password);
         users.add(user);
         return user;
-    }
-
-
-    public boolean validatePassword(String password) {
-        return password != null && password.length() >= 8 && password.length() <= 15;
     }
 
 

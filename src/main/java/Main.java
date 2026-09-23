98c6416716e6c97355f381975edffc88a3dcfa8b
Hannah Concillado <hannahconcillado@gmail.com>
2026-09-23 18:12:27 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,45 +20,6 @@ public class Main {
 
         }).start(7070);
 
-
-
-
-
-
-
-        /*try {
-            BufferedImage myPicture = ImageIO.read(new File("src/Map.png"));
-
-
-            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-
-
-            JFrame frame = new JFrame("Image Viewer");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.add(picLabel);
-
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FileIO fileIO = new FileIO();
-        TextUI textUI = new TextUI();
-
-        List<User> users = FileIO.loadUsers("Data/UserData");
-        List<Zone> zones = fileIO.loadZones("Data/zoneData");
-        List<Car> cars = fileIO.loadCars("Data/carData");
-
-        textUI.displayMsg("Velkommen til Parkeringsservice S&N");
-        Menu menu = new Menu(users, zones, cars, textUI, fileIO);
-
-        menu.start();
-    }*/
-
-
     }
 
 }
